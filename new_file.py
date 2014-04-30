7f653993d4662e80b1e7dea2cd8e5b40683e1992
stanvdbosch <stan.vandenbosch@nelen-schuurmans.nl>
2014-04-30 17:26:34 +0200

    eerste dagje python programmeren

--- new_file.py
+++ new_file.py
@@ -1,5 +1,20 @@
 #!/usr/bin/env python
 
+
+animals = ["aardvark", "badger", "duck", "emu", "fennec fox"]
+print animals.index("duck")
+animals.insert (2,"cobra")
+animals.remove("badger")
+print animals
+
+my_list = [1,9,3,8,5,7]
+my_list.sort() #werkt ook voor strings
+
+for number in my_list:
+	print number
+
+
+"""
 print("new")
 
 a = 1
@@ -7,4 +22,13 @@ b = 2
 c = [1, 3, 4]
 
 for item in c:
-    print(item)
+    print(item)"""
+
+var = {'Puffin' : 104, 'Sloth' : 105, 'Burmese Python' : 106}
+print var['Sloth']
+var['Aap']=107
+print var
+del var['Puffin']
+print var
+var['Sloth'] = "999"
+print var
